c4d77cab082eb1070da06ddbbc13742803e7b3d2
elbarbarosso510 <el_barbarosso510@protonmail.com>
2024-03-24 16:40:42 +0200

    improve performance

---
+++ enps.sql
@@ -0,0 +1,21 @@
+CREATE TABLE results (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   event text,
+   stat_type text,
+   year text,
+   month text,
+   place num, 
+   lname text, 
+   fname text,
+   uspsa text,
+   class text,
+   division text, 
+   pf text,
+   lady text,
+   mil text, 
+   law text, 
+   for text,
+   age text,
+   points num,
+   stgperc num
+);
